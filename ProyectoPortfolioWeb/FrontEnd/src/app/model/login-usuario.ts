e99b80918f40e58c9c51094b55214913441ab610
SVI73 <eliasbussanich@gmail.com>
2023-03-03 06:44:30 -0300

    Separación del Backend en su propio repo

--- ProyectoPortfolioWeb/FrontEnd/src/app/model/login-usuario.ts
+++
@@ -1,9 +0,0 @@
-export class LoginUsuario {
-    nombreUsuario: string = 'nombre';
-    password: string = '';
-
-    constructor(nombreUsuario: string, password: string) {
-        this.nombreUsuario = nombreUsuario;
-        this.password = password;
-    }
-}
